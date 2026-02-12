f1fad306209fee3538aa6ca0c49ea275b3abb8bd
Marchant01 <celvin.marchant@hotmail.com>
2026-02-12 12:35:35 +0100

    Initial commit with a poc for pulling pretrained models and use them

---
+++ main.py
@@ -0,0 +1,33 @@
+import onnxruntime as ort
+from transformers import AutoTokenizer
+from optimum.onnxruntime import ORTModelForCausalLM
+from onnxruntime import SessionOptions, GraphOptimizationLevel
+
+def main():
+    print("Hello from examensarbete!")
+    print("ORT version:", ort.__version__)
+    print("Providers:", ort.get_available_providers())
+
+    repo_id = "onnx-community/Qwen2.5-0.5B-Instruct"
+
+    so = ort.SessionOptions()
+    so.graph_optimization_level = GraphOptimizationLevel.ORT_ENABLE_LAYOUT
+
+    tokenizer = AutoTokenizer.from_pretrained(repo_id)
+
+    model = ORTModelForCausalLM.from_pretrained(
+        repo_id,
+        subfolder="onnx",
+        file_name="model_fp16.onnx",
+        providers=["CPUExecutionProvider"],
+        session_options=so,
+    )
+
+    prompt = "Skriv tre idéer för ett examensarbete om lokal AI."
+    inputs = tokenizer(prompt, return_tensors="pt")
+
+    out = model.generate(**inputs, max_new_tokens=80)
+    print(tokenizer.decode(out[0], skip_special_tokens=True))
+
+if __name__ == "__main__":
+    main()
