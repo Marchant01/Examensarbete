cfd53b752d22f8c89135580a17c4d3e3aac112fb
Marchant01 <celvin.marchant@hotmail.com>
2026-02-12 15:35:03 +0100

    Fixing git issues

--- model_loader.py
+++ model_loader.py
@@ -1,11 +1,11 @@
-from huggingface_hub import list_models
+from huggingface_hub import list_models, snapshot_download
 
+# Test version, loads available HF models based on filters and installs selected model.
 def select_model():
     print("Select a filter")
     
     model_filter = ["text-to-image", "text-generation", "image-to-image"]
-    for filters in model_filter:
-        print(filter)
+    print(*model_filter, sep=", ")
 
     selected = input()
     print("listing models based on fitler")
@@ -15,4 +15,7 @@ def select_model():
     for model in models:
         print(model)
 
-    
+    print("Select a model to isntall with repo_id")
+    repo_id = input()
+    snapshot_download(repo_id=repo_id, local_dir=f"models/{repo_id}")
+
