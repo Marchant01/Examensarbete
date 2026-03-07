7cb971878b6b1a1efacd9823669537895b7d26a0
Marchant01 <celvin.marchant@hotmail.com>
2026-03-07 19:07:37 +0100

    Adjusted git ignore to properly track models dir and onnx runtime being installed correctly by optimum

---
+++ app/app/src/routes/models/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Models from "$lib/components/models.svelte";
+</script>
+
+<main class="model-main">
+  <h1>Models</h1>
+  <div class='modelSelection'>
+    <Models />
+  </div>
+</main>
